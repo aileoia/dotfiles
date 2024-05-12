2f539f125b7a35507dd87ad3fb62c05b079d180d
Léo <leo@pitro.eu>
2024-05-12 15:25:15 +0200

    fix(nvim): adapt download link to arch

--- install-conda-and-some-more.zsh
+++ install-conda-and-some-more.zsh
@@ -1,17 +1,30 @@
 # Installs conda and additionnal python packages.
 
-# Download an install miniconda
+if [ $(uname -m) = "x86_64" ]
+then
+  NVIM_DL_LINK=https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+elif [ $(uname -m) = "aarch64" ]
+then
+  NVIM_DL_LINK=https://github.com/matsuu/neovim-aarch64-appimage/releases/download/v0.9.4/nvim-v0.9.4-aarch64.appimage
+fi
+echo "Download and intall neovim from ${NVIM_DL_LINK}"
+wget ${NVIM_DL_LINK} -O ~/.local/bin/nvim
+chmod +x ~/.local/bin/nvim
+
+echo "Download an install miniconda"
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh -O miniforge.sh
 sh miniforge.sh -b
 rm miniforge.sh
 
 source zshrc
 
+echo "Install neovim things"
 pip install --user pipx pynvim jedi
 
 conda install -y nodejs
 npm install -g neovim
 
+echo "Install a few python based things"
 pipx install black
 pipx install dvc"[all]"
 pipx install flake8
