32a4bcac1c2cd641fa7b766abce6ecd423072520
Léo Granger <leo.granger@elevait.de>
2024-09-16 12:42:17 +0200

    feat(nvim): add helm and docker pack

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -5,9 +5,11 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.helm" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.lsp.inc-rename-nvim" },
   -- import/override with your plugins folder
 }
